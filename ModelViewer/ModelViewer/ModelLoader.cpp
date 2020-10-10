b4ad77950510600807478e38022c35b7b00b0ef1
Kyle Hiebel <khiebel1001@gmail.com>
2020-10-10 11:17:06 -0700

    Added test for linking ASSIMP

---
+++ ModelViewer/ModelViewer/ModelLoader.cpp
@@ -0,0 +1,24 @@
+#include "ModelLoader.h"
+
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
+
+bool ModelLoader::LoadModel(const QString& file)
+{
+	// Create an instance of the Importer class
+	Assimp::Importer importer;
+	
+	// And have it read the given file with some example postprocessing
+	// Usually - if speed is not the most important aspect for you - you'll
+	// probably to request more postprocessing than we do in this example.
+	const aiScene* pScene = importer.ReadFile(file.toStdString(), aiProcess_CalcTangentSpace |aiProcess_Triangulate |aiProcess_JoinIdenticalVertices |aiProcess_SortByPType);
+	
+	// If the import failed, report it
+	if (!pScene) {
+		return false;
+	}
+
+	// We're done. Everything will be cleaned up by the importer destructor
+	return true;
+}
