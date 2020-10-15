d3d54351b4531ec6499ba5e019e62c13913dba8b
Kyle Hiebel <khiebel1001@gmail.com>
2020-10-15 10:40:08 -0700

    Fix aspect ration bug

--- ModelViewer/ModelViewer/ViewerGraphicsWindow.cpp
+++ ModelViewer/ModelViewer/ViewerGraphicsWindow.cpp
@@ -84,7 +84,7 @@ void ViewerGraphicsWindow::render()
     m_program->bind();
 
     QMatrix4x4 matrix;
-    matrix.perspective(60.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+    matrix.perspective(60.0f, (float)width() / (float)height(), 0.1f, 100.0f);
     matrix.translate(0, 0, -4);
     matrix.rotate(100.0f * m_frame / screen()->refreshRate(), 0, 1, 0);
 
