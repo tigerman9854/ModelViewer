985b35608173f6e1bbc4cc11a00b8496b3d0833e
richjoregonstate <richj@oregonstate.edu>
2020-10-23 10:33:00 -0700

    Fixed spaceing

--- ModelViewer/ModelViewer/ViewerGraphicsWindow.cpp
+++ ModelViewer/ModelViewer/ViewerGraphicsWindow.cpp
@@ -12,20 +12,20 @@
 
 // Define default shaders
 static const char* vertexShaderSource =
-"attribute highp vec4 posAttr;\n"
-"attribute lowp vec4 colAttr;\n"
-"varying lowp vec4 col;\n"
-"uniform highp mat4 matrix;\n"
-"void main() {\n"
-"   col = colAttr;\n"
-"   gl_Position = matrix * posAttr;\n"
-"}\n";
+    "attribute highp vec4 posAttr;\n"
+    "attribute lowp vec4 colAttr;\n"
+    "varying lowp vec4 col;\n"
+    "uniform highp mat4 matrix;\n"
+    "void main() {\n"
+    "   col = colAttr;\n"
+    "   gl_Position = matrix * posAttr;\n"
+    "}\n";
 
 static const char* fragmentShaderSource =
-"varying lowp vec4 col;\n"
-"void main() {\n"
-"   gl_FragColor = col;\n"
-"}\n";
+    "varying lowp vec4 col;\n"
+    "void main() {\n"
+    "   gl_FragColor = col;\n"
+    "}\n";
 
 
 ViewerGraphicsWindow::ViewerGraphicsWindow(QWindow* parent)
