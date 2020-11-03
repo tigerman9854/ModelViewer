3c56192c30affa7591ee2b03cae0287a17d56fce
richjoregonstate <richj@oregonstate.edu>
2020-11-02 18:12:31 -0800

    Menu & swaping are working (In Kyle's awsome delegate style), next step is setting up the core menus and figuring out live swaping of data

--- ModelViewer/ModelViewer/SettingsMenu.h
+++ ModelViewer/ModelViewer/SettingsMenu.h
@@ -18,14 +18,34 @@ public:
 //private slots:
 
 private:
+	enum settingsWidget {
+		mouse,
+		keybind,
+		shader,
+		model
+	};
+
 	ViewerGraphicsWindow* m_pGraphicsWindow = nullptr;
 	QGridLayout* m_pMainLayout = nullptr;
 	QListWidget* m_pSettingsList = nullptr;
 	QWidget* m_pCurrentSettingsWidget = nullptr;
 
 	// Settings menus
+	void SetupMouseSettings();
 	QWidget* m_pMouseSettings = nullptr;
+	QString m_MouseTitle = QString::fromLatin1("Mouse");
+
+	void SetupKeybindSettings();
 	QWidget* m_pKeybindSettings = nullptr;
+	QString m_KebindTitle = QString::fromLatin1("Keybindings");
+
+	void SetupShaderSettings();
 	QWidget* m_pShaderSettings = nullptr;
+	QString m_ShaderTitle = QString::fromLatin1("Shader");
+
+	void SetupModelSettings();
 	QWidget* m_pModelSettings = nullptr;
+	QString m_ModelTitle = QString::fromLatin1("Model");
+
+	
 };
